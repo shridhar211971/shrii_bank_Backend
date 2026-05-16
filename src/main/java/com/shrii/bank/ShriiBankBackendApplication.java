0b6391b7f1b11a4770bccd1964edd5d63683b0a7
shridhar-indi4 <shridhar.bhoslae@indi4.io>
2026-05-16 08:07:31 +0530

    Initial Spring Boot backend setup

---
+++ src/main/java/com/shrii/bank/ShriiBankBackendApplication.java
@@ -0,0 +1,13 @@
+package com.shrii.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShriiBankBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShriiBankBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shrii/bank/ShriiBankBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shrii.bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShriiBankBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
