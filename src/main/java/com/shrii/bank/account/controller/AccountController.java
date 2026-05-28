0cf7ec3689303462e2196639c94dff3df954a53e
shridhar-indi4 <shridhar.bhoslae@indi4.io>
2026-05-28 15:40:24 +0530

    added the validation file

--- src/main/java/com/shrii/bank/account/controller/AccountController.java
+++ src/main/java/com/shrii/bank/account/controller/AccountController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/accounts")
-@RequiredArgsConstructor
+//@RequiredArgsConstructor
 public class AccountController {
 
     private final AccountService accountService;
